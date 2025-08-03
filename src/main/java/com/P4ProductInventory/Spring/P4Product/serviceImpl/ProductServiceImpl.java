1d3cfc595af7c0ee1319862b51a6e3712148b7d8
jagtapK <jagtapkajal54@gmail.com>
2025-08-03 22:53:19 +0530

    Create method to save Product with details

--- src/main/java/com/P4ProductInventory/Spring/P4Product/serviceImpl/ProductServiceImpl.java
+++ src/main/java/com/P4ProductInventory/Spring/P4Product/serviceImpl/ProductServiceImpl.java
@@ -1,13 +1,22 @@
 package com.P4ProductInventory.Spring.P4Product.serviceImpl;
 
 import com.P4ProductInventory.Spring.P4Product.entity.Product;
+import com.P4ProductInventory.Spring.P4Product.repository.ProductRepository;
 import com.P4ProductInventory.Spring.P4Product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
     @Override
     public String saveProduct(Product product) {
 
-
-        return "";
+        Product saveProduct = productRepository.save(product);
+        return "Product Details saved";
     }
 }
