ae2241df1a7d3a874c4f5635ec80c327e60a4ab6
jagtapK <jagtapkajal54@gmail.com>
2025-08-03 23:06:51 +0530

    Fix Method

--- src/main/java/com/P4ProductInventory/Spring/P4Product/controller/ProductController.java
+++ src/main/java/com/P4ProductInventory/Spring/P4Product/controller/ProductController.java
@@ -1,6 +1,33 @@
 package com.P4ProductInventory.Spring.P4Product.controller;
 
+import com.P4ProductInventory.Spring.P4Product.entity.Product;
+import com.P4ProductInventory.Spring.P4Product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/addProduct")
+    public ResponseEntity<String> addProduct(@RequestBody List<Product> product){
+
+        product.forEach(product1 -> {
+            System.err.println(product1);
+            productService.saveProduct(product1);
+        });
+
+        return new ResponseEntity<>("Save details Successfully", HttpStatus.CREATED);
+    }
 
 }
