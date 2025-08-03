54ea658f9eb4f20afb0163078da6cb6fc6d33bad
jagtapK <jagtapkajal54@gmail.com>
2025-08-03 23:39:55 +0530

    Fix Methods

--- src/main/java/com/P4ProductInventory/Spring/P4Product/service/ProductService.java
+++ src/main/java/com/P4ProductInventory/Spring/P4Product/service/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductService{
+
     public String saveProduct(Product product);
 
     public List<Product> getAllProducts();
